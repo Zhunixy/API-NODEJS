8b58f28d2e9188f777532f5e591b0f47aba2c991
Zhunixy <kauan8643@gmail.com>
2025-10-22 12:53:33 -0300

    Adicionado tratamento de erros em todas as rotas

--- server.js
+++ server.js
@@ -10,39 +10,72 @@ app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
 
 // GET
 app.get("/main", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.status(200).json(users);
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    //tratamento de erro genérico para erros internos do servidor
+    if (error) {
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+    res.status(400).json({ error: "Erro ao buscar usuários" });
+  }
 });
 
 // POST
 app.post("/main", async (req, res) => {
-  const users = await prisma.user.create({
-    data: {
-      email: req.body.email,
-      name: req.body.name,
-      age: req.body.age,
-    },
-  });
-  res.status(200).json(users);
+  try {
+    const users = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    //esse P2002 é um código de erro específico do Prisma que indica uma violação de chave única,
+    // ou seja, quando você tenta inserir um valor duplicado em um campo que deve ser único, como o email neste caso.
+    if (error.code === "P2002") {
+      return res.status(409).json({ error: "Email já cadastrado" });
+    }
+
+    res.status(400).json({ error: "Erro ao criar usuário" });
+  }
 });
 
 // PUT
 app.put("/main/:id", async (req, res) => {
-  const users = await prisma.user.update({
-    where: { id: req.params.id },
-    data: {
-      email: req.body.email,
-      name: req.body.name,
-      age: req.body.age,
-    },
-  });
-  res.status(200).json(users);
+  try {
+    const users = await prisma.user.update({
+      where: { id: req.params.id },
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        age: req.body.age,
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    if (error.code === "P2002") {
+      return res.status(409).json({ error: "Email já cadastrado" });
+    }
+    res.status(400).json({ error: "Erro ao atualizar usuário" });
+  }
 });
 
 // DELETE
 app.delete("/main/:id", async (req, res) => {
-  await prisma.user.delete({
-    where: { id: req.params.id },
-  });
-  res.status(200).json({ message: "Usuário deletado" });
+  try {
+    await prisma.user.delete({
+      where: { id: req.params.id },
+    });
+    res.status(200).json({ message: "Usuário deletado" });
+  } catch (error) {
+    // O código de erro P2025 indica que o registro que você está tentando deletar não foi encontrado no banco de dados.
+    if (error.code === "P2025") {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
+    res.status(400).json({ error: "Erro ao deletar usuário" });
+  }
 });
